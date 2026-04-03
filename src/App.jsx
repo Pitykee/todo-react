efe09acbfa3d34be7ca662a7a93aa337c00db48b
Pityke <istvang16@gmail.com>
2026-04-03 10:56:39 +0200

    vercel bug

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 import { Container } from "react-bootstrap";
 
 // components
-import Form from "./components/form";
+import Form from "./components/Form";
 
 function App() {
   return (
